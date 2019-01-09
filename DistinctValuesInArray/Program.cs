4a49a9a9879e1301fe0bcf903b6a9a25a6c484f4
Chris Tacey-Green <chris@taceygreen.com>
2019-01-09 12:25:23 +0000

    Add project files.

---
+++ DistinctValuesInArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+// you can also use other imports, for example:
+// using System.Collections.Generic;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+class Solution
+{
+    public int solution(int[] A)
+    {
+        // write your code in C# 6.0 with .NET 4.5 (Mono)
+        HashSet<int> aValues = new HashSet<int>();
+        foreach (var item in A)
+        {
+            if (!aValues.Contains(item))
+            {
+                aValues.Add(item);
+            }
+        }
+        return aValues.Count;
+    }
+}
